807fdbda78035ca61146746b0500ce53ef05dba7
Jordan Byron <jordan.byron@betterup.co>
2024-02-26 11:28:02 -0500

    chore: Run prettier on files

--- index.js
+++ index.js
@@ -1,78 +1,72 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-import get from 'lodash/get.js';
+import get from "lodash/get.js";
 
 import {
-    getAutomationNodesGraphQL,
-    getAutomationTemplateGraphQL,
-    getHeaders,
-    saveAutomationV2GraphQL,
-    saveAutomationV2TemplateQl,
-} from './utils.js';
-import { authorizations, graphqlEndpoint } from './variables.js';
+  getAutomationNodesGraphQL,
+  getAutomationTemplateGraphQL,
+  getHeaders,
+  saveAutomationV2GraphQL,
+  saveAutomationV2TemplateQl,
+} from "./utils.js";
+import { authorizations, graphqlEndpoint } from "./variables.js";
 
 const getAutomation = async (environment, locale, template_id) => {
-    const headers = getHeaders(locale);
-    const body_template = getAutomationTemplateGraphQL(template_id);
-    const body_nodes = getAutomationNodesGraphQL(template_id);
+  const headers = getHeaders(locale);
+  const body_template = getAutomationTemplateGraphQL(template_id);
+  const body_nodes = getAutomationNodesGraphQL(template_id);
 
-    const response_graph = await fetch(graphqlEndpoint(environment)[locale], {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(body_template),
-    });
+  const response_graph = await fetch(graphqlEndpoint(environment)[locale], {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body_template),
+  });
 
-    const template = await response_graph.json();
-    const response_nodes = await fetch(graphqlEndpoint(environment)[locale], {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(body_nodes),
-    });
-    const nodes = await response_nodes.json();
-    return {
-        template: get(template, 'data.automationsV2.template', {}),
-        nodes: get(nodes, 'data.automationsV2.nodes', []),
-    };
+  const template = await response_graph.json();
+  const response_nodes = await fetch(graphqlEndpoint(environment)[locale], {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body_nodes),
+  });
+  const nodes = await response_nodes.json();
+  return {
+    template: get(template, "data.automationsV2.template", {}),
+    nodes: get(nodes, "data.automationsV2.nodes", []),
+  };
 };
 
 const updateVariables = (content) => {
-  return content.replace(
-            /api\.courier\.com/,
-            'api.eu.courier.com'
-        );
-}
+  return content.replace(/api\.courier\.com/, "api.eu.courier.com");
+};
 
 const updateAutomation = async (environment, locale, nodes, template) => {
-    const headers = getHeaders(locale);
-    const body_nodes = saveAutomationV2GraphQL(nodes, template);
-    const body_template = saveAutomationV2TemplateQl(nodes, template);
-    // console.log('body_nodes', body_nodes.variables);
-    const response_nodes = await fetch(graphqlEndpoint(environment)[locale], {
-        method: 'POST',
-        headers,
-        body: updateVariables(JSON.stringify(body_nodes))
-    });
-    const response_template = await fetch(
-        graphqlEndpoint(environment)[locale],
-        {
-            method: 'POST',
-            headers,
-            body: updateVariables(JSON.stringify(body_template))
-        }
-    );
-    return {
-        nodes: await response_nodes.json(),
-        template: await response_template.json(),
-    };
+  const headers = getHeaders(locale);
+  const body_nodes = saveAutomationV2GraphQL(nodes, template);
+  const body_template = saveAutomationV2TemplateQl(nodes, template);
+  // console.log('body_nodes', body_nodes.variables);
+  const response_nodes = await fetch(graphqlEndpoint(environment)[locale], {
+    method: "POST",
+    headers,
+    body: updateVariables(JSON.stringify(body_nodes)),
+  });
+  const response_template = await fetch(graphqlEndpoint(environment)[locale], {
+    method: "POST",
+    headers,
+    body: updateVariables(JSON.stringify(body_template)),
+  });
+  return {
+    nodes: await response_nodes.json(),
+    template: await response_template.json(),
+  };
 };
 
 const syncAutomations = async (environment) => {
-    const automations = await fetch(graphqlEndpoint(environment)['us'], {
-        headers: getHeaders('us'),
-        method: 'POST',
-        body: JSON.stringify({
-            variables: {},
-            query: `{
+  const automations = await fetch(graphqlEndpoint(environment)["us"], {
+    headers: getHeaders("us"),
+    method: "POST",
+    body: JSON.stringify({
+      variables: {},
+      query: `{
                   automationTemplates {
                     nodes {
                       name
@@ -95,54 +89,49 @@ const syncAutomations = async (environment) => {
           }
         }
             `,
-        }),
-    });
-    const automation_data = await automations.json();
-    const automation_ids = get(
-        automation_data,
-        ['data', 'automationsV2', 'templates', 'templates'],
-        []
-    );
-    for (let i = 0; i < automation_ids.length; i++) {
-        const automation = automation_ids[i];
+    }),
+  });
+  const automation_data = await automations.json();
+  const automation_ids = get(
+    automation_data,
+    ["data", "automationsV2", "templates", "templates"],
+    [],
+  );
+  for (let i = 0; i < automation_ids.length; i++) {
+    const automation = automation_ids[i];
 
-        const { template, nodes } = await getAutomation(
-            environment,
-            'us',
-            automation.id
-        );
-        const saved = await updateAutomation(
-            environment,
-            'eu',
-            nodes,
-            template
-        );
-        console.log(
-            `Saved - ${get(saved, [
-                'template',
-                'data',
-                'automationsV2',
-                'saveTemplate',
-                'name',
-            ])} - (${automation.id})`
-        );
-    }
+    const { template, nodes } = await getAutomation(
+      environment,
+      "us",
+      automation.id,
+    );
+    const saved = await updateAutomation(environment, "eu", nodes, template);
+    console.log(
+      `Saved - ${get(saved, [
+        "template",
+        "data",
+        "automationsV2",
+        "saveTemplate",
+        "name",
+      ])} - (${automation.id})`,
+    );
+  }
 };
 
 // check JWT expiration
 const checkJWTExpiration = (locale) => {
-    const payload = JSON.parse(
-        Buffer.from(authorizations[locale].split('.')[1], 'base64').toString(
-            'utf-8'
-        )
-    );
-    const now = Math.floor(Date.now() / 1000);
-    if (payload.exp < now) {
-        throw Error('JWT expired: ' + locale);
-    }
+  const payload = JSON.parse(
+    Buffer.from(authorizations[locale].split(".")[1], "base64").toString(
+      "utf-8",
+    ),
+  );
+  const now = Math.floor(Date.now() / 1000);
+  if (payload.exp < now) {
+    throw Error("JWT expired: " + locale);
+  }
 };
 
-checkJWTExpiration('us');
-checkJWTExpiration('eu');
+checkJWTExpiration("us");
+checkJWTExpiration("eu");
 
-syncAutomations('test');
+syncAutomations("test");
--- variables.js
+++ variables.js
@@ -1,32 +1,32 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const tenant_id = process.env.TENANT_ID;
 export const domains = {
-    us: 'api.courier.com',
-    eu: 'api.eu.courier.com',
+  us: "api.courier.com",
+  eu: "api.eu.courier.com",
 };
 export const authorizations = {
-    us: process.env.US_AUTHORIZATION,
-    eu: process.env.EU_AUTHORIZATION,
+  us: process.env.US_AUTHORIZATION,
+  eu: process.env.EU_AUTHORIZATION,
 };
 export const graphqlEndpoint = (environment) => ({
-    us: `https://${domains.us}/studio/q?tenantId=${tenant_id}${
-        environment === 'test' ? '&env=test' : ''
-    }`,
-    eu: `https://${domains.eu}/studio/q?tenantId=${tenant_id}${
-        environment === 'test' ? '&env=test' : ''
-    }`,
+  us: `https://${domains.us}/studio/q?tenantId=${tenant_id}${
+    environment === "test" ? "&env=test" : ""
+  }`,
+  eu: `https://${domains.eu}/studio/q?tenantId=${tenant_id}${
+    environment === "test" ? "&env=test" : ""
+  }`,
 });
 
 export const getRestEndpoint = (environment, path, query = {}) => {
-    const params = new URLSearchParams({ ...query, tenantId: tenant_id });
-    return {
-        us: `https://api.courier.com/studio${
-            environment === 'test' ? '/test' : ''
-        }${path}?${params.toString()}`,
-        eu: `https://api.eu.courier.com/studio${
-            environment === 'test' ? '/test' : ''
-        }${path}?${params.toString()}`,
-    };
+  const params = new URLSearchParams({ ...query, tenantId: tenant_id });
+  return {
+    us: `https://api.courier.com/studio${
+      environment === "test" ? "/test" : ""
+    }${path}?${params.toString()}`,
+    eu: `https://api.eu.courier.com/studio${
+      environment === "test" ? "/test" : ""
+    }${path}?${params.toString()}`,
+  };
 };
